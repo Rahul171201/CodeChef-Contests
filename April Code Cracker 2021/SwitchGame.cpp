d8e7b0b7459bd53794972e99dfa484d57a9314c5
Rahul Roy <70642284+Rahul171201@users.noreply.github.com>
2021-04-30 13:48:06 +0530

    Added solution to Switch Game

---
+++ April Code Cracker 2021/SwitchGame.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mod 1000000007
+#define ll long long int
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+
+        for (ll i = 1; i * i <= n; i++)
+        {
+            cout << i * i << " ";
+        }
+        cout << "\n";
+    }
+}
