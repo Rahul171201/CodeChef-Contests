a55d569e2c6dad8f4a997a9a61f780757513d4c2
Rahul Roy <70642284+Rahul171201@users.noreply.github.com>
2021-06-01 23:15:14 +0530

    Added solution to Make it Zero

---
+++ May Code Shake 2021/MakeItZero.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mod 1000000007
+#define ll long long int
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll a, b;
+        cin >> a >> b;
+
+        if (a == 0 || b==0)
+        {
+            cout << "NO"
+                 << "\n";
+        }
+        else
+        {
+            cout << "YES"
+                 << "\n";
+        }
+    }
+}
