eb4682014c38739a74d7395e283c9f7250fbfcc9
Rahul Roy <70642284+Rahul171201@users.noreply.github.com>
2021-07-14 13:04:55 +0530

    Added solution to Hoop Jump

---
+++ May Lunchtime 2021/HoopJump.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mod 1000000007
+#define ll long long int
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        ll ans = (n / 2) + 1;
+        cout << ans << "\n";
+    }
+}
